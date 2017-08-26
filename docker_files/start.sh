724ce6dc85b5db14d560d7f7265be659311a4802
Adrian Goins <mon@chus.cc>
2017-08-26 17:03:43 -0300

    add startup opts in var

--- docker_files/start.sh
+++ docker_files/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/usr/local/bin/mysqldump -- "$@"
+if [[ ! -z ${STARTUP_OPTIONS} ]]; then
+  /usr/local/bin/mysqldump ${STARTUP_OPTIONS} -- "$@"
+else
+  /usr/local/bin/mysqldump -- "$@"
+fi
